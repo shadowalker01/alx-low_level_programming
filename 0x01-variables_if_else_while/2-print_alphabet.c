a9ec04f0c8af096974fddfea260025df9994c3b3
shadowalker01 <kevinskigathege@gmail.com>
2021-08-30 13:52:06 -0700

    for loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char letter;
+
+  for (letter = 'a'; letter <= 'z'; letter++)
+          putchar(letter)
+
+  putchar('\n');
+
+  return (0);
+}
+
