91e6e23a048033478cacf0e4615b89504d09a5c1
shadowalker01 <kevinskigathege@gmail.com>
2021-08-31 08:35:48 -0700

    a C program that prints Holberton, followed by a new line. Returns 0.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-  char *sh = "Holberton";
+char *sh = "Holberton";
 
-  while (*sh)
-    {
-      _putchar(*sh);
-      sh++;
-    }
-  _putchar('\n');
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
 
-  return (0);
+return (0);
 }
