3993fee094ec033c583d4c25828f3139aa80fd2f
shadowalker01 <kevinskigathege@gmail.com>
2021-10-07 13:51:31 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_start - print_start.
+*
+* Return: Nothing.
+*/
+void print_start(void)
+{
+char *s1 = "You're beat! and yet, you must allow,\n";
+char *s2 = "I bore my house upon my back!\n";
+
+printf("%s%s", s1, s2);
+}
